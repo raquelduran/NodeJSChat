4c6a7b9975d7f82889c42ca47b63118b6dd6f5de
Raquel <Raquel>
2017-02-20 12:09:08 +0100

    changed port for herokuapp

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + "/index.html");
-});
+app.use(express.static('/'));
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + "/index.html");
+// });
 
 io.on('connection', function(socket){
-	console.log('a user connected');
 	socket.on('chat message', function(msg){
-   		console.log('message: '+ msg);
    		io.sockets.emit('chat message', msg);
   	});
 })
 
-http.listen(3000, function(){
+
+const PORT = process.env.PORT || '3000'
+
+http.listen(PORT, function(){
   console.log('listening on *:3000');
 });
